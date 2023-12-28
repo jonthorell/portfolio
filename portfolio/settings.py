41df29b95072836ce2e3b106d35c9fdbc7ce314b
Jonas Thorell <jthorell@gmail.com>
2023-12-28 03:23:40 +0100

    db change

--- portfolio/settings.py
+++ portfolio/settings.py
@@ -106,18 +106,18 @@ WSGI_APPLICATION = 'portfolio.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.sqlite3',
-#         'NAME': 'db.sqlite3',
-#     }
-# }
-
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': 'db.sqlite3',
+    }
 }
 
-DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
+# DATABASES = {
+#     'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+# }
+
+# DATABASES['default']['ENGINE'] = 'django.db.backends.sqlite3'
 
 
 # Password validation
