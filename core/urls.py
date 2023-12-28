0955162baedbcdf29a05f51c2b133c40456fe7e2
Jonas Thorell <jthorell@gmail.com>
2023-12-28 01:53:27 +0100

    Add: breadcrumb fully operational. Placeholder templates present for all projects. Table showing all projects present.

--- core/urls.py
+++ core/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     path("", views.index.as_view(), name="index"),
+    path("encyclopedia", views.encyclopedia.as_view(), name="encyclopedia"),
+    path("pirate_island", views.pirate_island.as_view(), name="pirate_island"),
+    path("quiz", views.quiz.as_view(), name="quiz"),
+    path("retro", views.retro.as_view(), name="retro"),
+    path("projects", views.projects.as_view(), name="projects"),
+    path("synth", views.synth.as_view(), name="synth"),
+    path("portfolio", views.portfolio.as_view(), name="portfolio"),
 ]
--- core/views.py
+++ core/views.py
@@ -8,3 +8,38 @@ class index(TemplateView):
     '''Class used to display the index page '''
 
     template_name = 'core/index.html'
+    
+class encyclopedia(TemplateView):
+    '''Class used to display the encyclopedia page '''
+
+    template_name = 'core/encyclopedia.html'
+    
+class projects(TemplateView):
+    '''Class used to display the projects page '''
+
+    template_name = 'core/projects.html'
+    
+class quiz(TemplateView):
+    '''Class used to display the quiz page '''
+
+    template_name = 'core/quiz.html'
+    
+class pirate_island(TemplateView):
+    '''Class used to display the Pirate Island page '''
+
+    template_name = 'core/pirate_island.html'
+    
+class retro(TemplateView):
+    '''Class used to display the RetroLovers page '''
+
+    template_name = 'core/retro.html'
+    
+class synth(TemplateView):
+    '''Class used to display the synth.nu page '''
+
+    template_name = 'core/synth.html'
+    
+class portfolio(TemplateView):
+    '''Class used to display the portfolio page '''
+
+    template_name = 'core/portfolio.html'
