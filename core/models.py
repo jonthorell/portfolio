a81cf7d27ac445c688a92fa6ea42309a5500ffc7
Jonas Thorell <jthorell@gmail.com>
2024-01-18 07:45:13 +0100

    Add: dbase connection and autoslug

--- core/models.py
+++ core/models.py
@@ -1,3 +1,23 @@
+from django.db.models import CharField, Model
 from django.db import models
+from autoslug import AutoSlugField
 
 # Create your models here.
+
+class project(models.Model):
+    '''Class used to create the projects model '''
+
+    title = models.CharField(max_length=60, blank=False, null=False, unique=True)
+    slug = AutoSlugField(populate_from='title', unique=True)
+    description = models.CharField(max_length=255, blank=False, null=False)
+    updated_on = models.DateTimeField(auto_now=True)
+    created_on = models.DateTimeField(auto_now_add=True)
+
+    class Meta:
+        ordering = ['title']
+
+    def __str__(self):
+        return self.title
+    
+    def get_status(self):
+        return self.status
--- static/javascript/script.js
+++ static/javascript/script.js
@@ -1,2 +1,11 @@
 /* empty file*/
-/* empty file*/
+
+document.addEventListener('DOMContentLoaded', function () {
+    //let box = document.getElementById('box');
+    //box.style.backgroundColor = 'lightpink';
+    document.getElementById('box').style.backgroundColor = 'lightpink';
+}, false);
+function turnPink() {
+    let box = document.getElementById('box');
+    box.style.backgroundColor = 'lightpink';
+}
